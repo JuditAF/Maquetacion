9daccb32a3a1b2e112cf51241c3012a830ccabc1
JuditAF <idryscreep@gmail.com>
2024-04-25 20:08:58 +0200

    Subo los archivos correspondientes a la página de la Agencia de Viajes sin terminar

---
+++ Reto_MaquetacionJsVanilla/agenciaViajes.js
@@ -0,0 +1,28 @@
+
+function mostrarOcultarInfo () {
+    let info = document.getElementById("infoRecibida");
+    if (info.style.display === "none") {
+        info.style.display = "block";
+    } else {
+        info.style.display = "none";
+    }
+}
+
+class Cliente {
+    constructor() {
+        this.name= " ";
+        this.persons= 0;
+        this.animalFriend= False;
+        this.date= new Date();
+        this.origin= " ";
+        this.destiny= " ";
+    }
+}
+
+let consultaClientes = [...Object];
+
+for (let dato of Cliente) {
+    return consultaClientes.push(dato);
+}
+
+let cliente1 = new Cliente(document.getElementById(nombre), document.getElementById(numeroViajeros), document.getElementById(amigoAnimal), document.getElementById(fecha), document.getElementById(origen), document.getElementById(destino));
